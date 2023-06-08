917124b5092c565432b1632955361edad9dddf78
Vinicius Philot <vphilot@gmail.com>
2023-06-08 09:06:01 -0400

    chore: :speech_balloon: update job title and description

--- src/pages/work.vue
+++ src/pages/work.vue
@@ -11,24 +11,16 @@ useHead({
   <div class="w-full md:w-2/3 text-lg tracking-wide leading-relaxed">
     <section>
       <h1 class="text-4xl md:text-7xl tracking-wide mb-8 font-sans bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-green-600 dark:from-blue-500 dark:to-green-400">
-        Currently: Lead Front-end Developer at
-        <Link title="Creative Layer" url="https://www.creativelayer.com/" class="" :dashed="false">
+        Currently: Senior Front-end Developer for
+        <Link title="RBC" url="https://www.rbcroyalbank.com/" class="" :dashed="false">
           <!-- Safari hack -->
           <span class="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-green-600 dark:from-blue-500 dark:to-green-400">
-            Creative Layer
+            RBC
           </span>
         </Link>
       </h1>
       <p class="mb-4">
-        I joined CL in early 2021 as a remote engineer, working from Toronto with an amazing team of developers located all over Ontario. We build tools to empower creators by giving them access to new revenue streams.
-      </p>
-      <p class="mb-4">
-        Our first platform, <em>Workflow</em>, focused on the print-on-demand market by making it easy for merchants and artists to communicate, review, and ship their work.
-      </p>
-      <p>
-        We are now tapping into the Web3 space with <em> <Link title="Remx" url="https://remx.xyz/">
-          Remx
-        </Link></em>, a digital creative community focused on art, culture, & identity.
+        Currently working for RBC's Global Cyber Security team building internal web apps that help us keep things our users safe using Vue3, Vite, and Tailwind.
       </p>
     </section>
     <DividerY />
@@ -36,7 +28,38 @@ useHead({
     <section>
       <div class="flex items-center space-x-4 mb-4">
         <h1 class="text-3xl md:text-4xl font-thin tracking-wide font-sans">
-          Prev: Front-end Software Developer at Cineplex
+          Prev: Lead Front-end Developer for <Link title="Creative Layer" url="https://www.creativelayer.com/" class="" :dashed="false">
+            <!-- Safari hack -->
+            <span class="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-green-600 dark:from-blue-500 dark:to-green-400">
+              Creative Layer
+            </span>
+          </Link>
+        </h1>
+      </div>
+      <ul class="pl-4 list-disc">
+        <li> Joined CL in early 2021 as a remote engineer, working from Toronto with a diverse team of developers located all over Ontario. We built tools to empower creators by giving them access to new revenue streams</li>
+        <li>Reported to the Director of Technology </li>
+        <li>Bootstrapped web apps using Vue3, Vite, Tailwind, and a GraphQL layer backed by an AWS serverless stack </li>
+        <li>Set up a 3d model parsing and rendering workflow with Blender, ThreeJS, and AWS Lambda to generate and export assets in the cloud</li>
+        <li>Deep dived into the world of writing custom Shaders with GLSL which was both a huge headache and a fantastic journey at the same time</li>
+        <li>Our first platform, <em>Workflow</em>, focused on the print-on-demand market by making it easy for merchants and artists to communicate, review, and ship their work.</li>
+        <li>
+          We then tapped into the Web3 space with <em> <Link title="Remx" url="https://remx.xyz/">
+            Remx
+          </Link></em>, a digital creative community focused on art, culture, & identity.
+        </li>
+      </ul>
+    </section>
+    <DividerY />
+    <section>
+      <div class="flex items-center space-x-4 mb-4">
+        <h1 class="text-3xl md:text-4xl font-thin tracking-wide font-sans">
+          Prev: Front-end Software Developer for <Link title="Cineplex Entertainment" url="https://www.cineplex.com/" class="" :dashed="false">
+            <!-- Safari hack -->
+            <span class="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-green-600 dark:from-blue-500 dark:to-green-400">
+              Cineplex Entertainment
+            </span>
+          </Link>
         </h1>
       </div>
       <ul class="pl-4 list-disc">
